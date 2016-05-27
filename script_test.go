f135470c19f393567509794e7ad9d39c5dc3c3e9
shaalx <shaalx@yahoo.com.sg>
2016-05-27 22:33:08 +0800

    auto

--- script_test.go
+++ script_test.go
@@ -4,15 +4,16 @@ import (
 	"fmt"
 	"github.com/astaxie/beego/httplib"
 	"github.com/shaalx/goutils"
+	"testing"
 	"time"
 )
 
-func main() {
-	test("github.com")
-	test("facebook.com")
+func TestHTTPS(t *testing.T) {
+	https_("github.com")
+	https_("facebook.com")
 }
 
-func test(uri string) {
+func https_(uri string) {
 	str, err := httplib.Get(fmt.Sprintf("https://%s", uri)).SetTimeout(3*time.Second, 2*time.Second).String()
 	goutils.CheckErr(err)
 	fmt.Println(str)
