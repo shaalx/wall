dfed8eb18c413be26704282460f18db446e80900
shaalx <shaalx@yahoo.com.sg>
2016-05-31 01:30:48 +0800

    auto

--- main.go
+++ main.go
@@ -2,46 +2,67 @@ package main
 
 import (
 	"fmt"
+	"github.com/everfore/rpcsv"
 	"github.com/shaalx/goutils"
 	"github.com/shaalx/wall/httplib"
-	"net/http"
+	"io/ioutil"
 	"time"
 )
 
+var (
+	rpc_tcp_server = "tcphub.t0.daoapp.io:61142"
+	// rpc_tcp_server = ":8800"
+)
+
 func main() {
-	// test()
-	// return
-	http.HandleFunc("/http/", suffer)
-	http.HandleFunc("/https/", https)
-	http.ListenAndServe(":80", nil)
+	WallLoop()
 }
 
-func suffer(rw http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
-	fmt.Println(req.Form, req.RequestURI)
-	uri := req.RequestURI
-	fmt.Println(uri[6:])
-	resp := httplib.Get(fmt.Sprintf("http://%s", uri[6:])).SetTimeout(3*time.Second, 2*time.Second)
-	bs, err := resp.Bytes()
-	goutils.CheckErr(err)
-	fmt.Println(goutils.ToString(bs))
-	fmt.Fprint(rw, goutils.ToString(bs))
+func WallLoop() {
+	in := make([]byte, 1)
+	clt := rpcsv.RPCClientWithCodec(rpc_tcp_server)
+	defer clt.Close()
+	out := rpcsv.Job{}
+	for {
+		err := clt.Call("RPC.Wall", &in, &out)
+		if goutils.CheckErr(err) {
+			time.Sleep(1e9)
+			clt = rpcsv.RPCClientWithCodec(rpc_tcp_server)
+			continue
+		}
+		fmt.Println("Wall-result:", out)
+		b := https_(out.Target)
+		Upload(b, out.Name)
+		out.Result = b
+		ret := make([]byte, 1)
+		err = clt.Call("RPC.WallBack", &out, &ret)
+		goutils.CheckErr(err)
+		out.Result = nil
+		time.Sleep(1e9)
+	}
+
 }
 
-func https(rw http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
-	fmt.Println(req.Form, req.RequestURI)
-	uri := req.RequestURI
-	fmt.Println(uri[7:])
-	resp := httplib.Get(fmt.Sprintf("https://%s", uri[7:])).SetTimeout(3*time.Second, 2*time.Second)
-	bs, err := resp.Bytes()
-	goutils.CheckErr(err)
-	fmt.Println(goutils.ToString(bs))
-	fmt.Fprint(rw, goutils.ToString(bs))
+func https_(uri string) []byte {
+	b, err := httplib.Get(fmt.Sprintf("%s", uri)).SetTimeout(3*time.Second, 2*time.Second).Bytes()
+	if goutils.CheckErr(err) {
+		return goutils.ToByte(err.Error())
+	}
+	// fmt.Println(goutils.ToString(b))
+	return b
 }
 
-func test() {
-	str, err := httplib.Get("http://bookmark.daoapp.io").String()
-	goutils.CheckErr(err)
-	fmt.Println(str)
+func Upload(bs []byte, page string) (err error) {
+	req := httplib.Put(fmt.Sprintf("http://upload.daoapp.io/upload/%s.html", page))
+	req.Body(bs)
+	resp, err := req.DoRequest()
+	if goutils.CheckErr(err) {
+		return
+	}
+	rb, err := ioutil.ReadAll(resp.Body)
+	if goutils.CheckErr(err) {
+		return
+	}
+	fmt.Println(goutils.ToString(rb))
+	return nil
 }
